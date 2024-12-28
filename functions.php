c04bbea95913422b486f21e4d5fce59e4b6c85a3
ryomaruyama <marusin618@gmail.com>
2024-12-28 11:48:44 +0900

    [change]cssを読み込ませる。

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
+<?php
 function mytheme_enqueue_styles() {
     wp_enqueue_style('normalize-style', get_template_directory_uri() . '/assets/css/normalize.css');
-    wp_enqueue_style('style', get_stylesheet_uri()); 
+    wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
 }
 add_action('wp_enqueue_scripts', 'mytheme_enqueue_styles');
+?>
