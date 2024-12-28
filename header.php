2125bf93b60e9f124683d1343a0b164b7dfc3ccb
ryomaruyama <marusin618@gmail.com>
2024-12-28 17:27:23 +0900

    [change]containerを設定

--- header.php
+++ header.php
@@ -1,29 +1,28 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo( 'name' ); ?></title>
-    <meta name="description" content="<?php bloginfo( 'description' ); ?>">
-    
+    <title><?php bloginfo('name'); ?></title>
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+
     <!-- スタイルシートを読み込む -->
     <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
     <!-- ヘッダー部分 -->
     <header>
         <div class="site-header">
             <div class="logo">
-                <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-                   <h1 class="main-logo-title">わいあい広告</h1>
-                   <h2 class="sub-logo-title">行きつけのお得をもっと身近に！</h2>
+                <a href="<?php echo esc_url(home_url('/')); ?>">
+                    <h1 class="main-logo-title">わいあい広告</h1>
+                    <h2 class="sub-logo-title">行きつけのお得をもっと身近に！</h2>
                 </a>
             </div>
-
-            <div class="posting-button">
-                <a href="<?php echo esc_url( home_url ('/') ) ?>">
-                    <span>掲載希望の方はこちら →</span>
-                </a>
-            </div>
+            <a class="posting-button" href="<?php echo esc_url(home_url('/')) ?>">
+                <span>掲載希望の方はこちら →</span>
+            </a>
         </div>
     </header>
