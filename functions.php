0205768c2c89081da26afc8103e6410c80405fe5
ryomaruyama <marusin618@gmail.com>
2024-12-28 11:32:05 +0900

    [change]normalize.cssを読み込み

---
+++ functions.php
@@ -0,0 +1,5 @@
+function mytheme_enqueue_styles() {
+    wp_enqueue_style('normalize-style', get_template_directory_uri() . '/assets/css/normalize.css');
+    wp_enqueue_style('style', get_stylesheet_uri()); 
+}
+add_action('wp_enqueue_scripts', 'mytheme_enqueue_styles');
