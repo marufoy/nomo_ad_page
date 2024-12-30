aec9f3f791c4f9af234d740181db1207ac909e37
ryomaruyama <marusin618@gmail.com>
2024-12-30 13:36:44 +0900

    [change]最新投稿の画像の大きさを調整

--- front-page.php
+++ front-page.php
@@ -2,65 +2,71 @@
 
 <div class="latest-info">
     <h2>最新情報</h2>
-    <?php
-
-    $args = array(
-        'post_type' => 'shop_info',
-        'posts_per_page' => 5, // 最新5件を取得
-    );
-    $latest_shops = new WP_Query($args);
-
-    if ($latest_shops->have_posts()) :
-        while ($latest_shops->have_posts()) : $latest_shops->the_post();
-            // 各店舗のタイトルとサムネイルを表示
-            the_title('<h3>', '</h3>');
-           
-            // ACFのサムネイル画像を取得する
-            $thumbnail = get_field('store_image');
-            if ($thumbnail) :
-                ?>
-                <div class="latest-info-thumbnail">
-                <img src="<?php echo esc_url($thumbnail['url']); ?>" alt="<?php echo esc_attr($thumbnail['alt']); ?>">
-                </div>
-                <?php
-            else :
-                // 画像がない場合はデフォルト画像を表示
-                ?>
-                <img src="https://example.com/default-image.jpg" alt="デフォルト画像">
-                <?php
-            endif;
-        endwhile;
-        wp_reset_postdata();
-    endif;
-    ?>
-
-<div class="genre-container">
-    <h2>掲載ジャンル</h2>
-    <div class="genre-grid">
+    <div class="latest-info-container">
         <?php
-        $genres = get_terms(array(
-            'taxonomy' => 'genre',
-            'hide_empty' => false,
-        ));
+        $args = array(
+            'post_type' => 'shop_info',
+            'posts_per_page' => 5, // 最新5件を取得
+        );
+        $latest_shops = new WP_Query($args);
 
+        if ($latest_shops->have_posts()):
+            while ($latest_shops->have_posts()):
+                $latest_shops->the_post();
 
-        foreach ($genres as $genre) :
-         // ACFで設定したカスタムフィールドから画像URLを取得
-         $genre_image = get_field('genre_image', $genre);
+                // ACFのサムネイル画像を取得
+                $thumbnail = get_field('store_image');
+                ?>
+                <div class="latest-info-item">
+                    <?php if ($thumbnail): ?>
+                        <div class="latest-info-thumbnail">
+                            <img src="<?php echo esc_url($thumbnail['url']); ?>" alt="<?php echo esc_attr($thumbnail['alt']); ?>">
+                        </div>
+                    <?php else: ?>
+                        <div class="latest-info-thumbnail">
+                            <img src="https://example.com/default-image.jpg" alt="デフォルト画像">
+                        </div>
+                    <?php endif; ?>
 
-         if(!$genre_image) {
-            $genre_image = "http://adpagedev.local/wp-content/uploads/2024/12/keyboard-6753584_1280.jpg";
-         }
-
-        ?>
-            <a href="<?php echo get_term_link($genre); ?>" class="genre-item">
-                <div class="genre-image" style="background-image: url('<?php echo $genre_image ?>');">
-                    <p><?php echo esc_html($genre->name); ?></p>
+                    <?php // タイトルを表示 ?>
+                    <h3><?php the_title(); ?></h3>
                 </div>
-            </a>
-        <?php endforeach; ?>
+                <?php
+            endwhile;
+            wp_reset_postdata();
+        endif;
+        ?>
     </div>
 </div>
+
+
+    <div class="genre-container">
+        <h2>掲載ジャンル</h2>
+        <div class="genre-grid">
+            <?php
+            $genres = get_terms(array(
+                'taxonomy' => 'genre',
+                'hide_empty' => false,
+            ));
+
+
+            foreach ($genres as $genre):
+                // ACFで設定したカスタムフィールドから画像URLを取得
+                $genre_image = get_field('genre_image', $genre);
+
+                if (!$genre_image) {
+                    $genre_image = "http://adpagedev.local/wp-content/uploads/2024/12/keyboard-6753584_1280.jpg";
+                }
+
+                ?>
+                <a href="<?php echo get_term_link($genre); ?>" class="genre-item">
+                    <div class="genre-image" style="background-image: url('<?php echo $genre_image ?>');">
+                        <p><?php echo esc_html($genre->name); ?></p>
+                    </div>
+                </a>
+            <?php endforeach; ?>
+        </div>
+    </div>
 
 
 </div>
