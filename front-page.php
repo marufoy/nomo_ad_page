590978233ef5b1662515007f7fe54d929dc9d65d
ryomaruyama <marusin618@gmail.com>
2025-01-03 16:19:09 +0900

    [change]それぞれの画像にリンクを付与

--- front-page.php
+++ front-page.php
@@ -5,30 +5,32 @@
     <div class="latest-info-container">  <!-- Slickが適用されるコンテナ -->
         <?php
         $args = array(
-            'post_type' => 'shop_info',
-            'posts_per_page' => 5,
+            'post_type' => 'shop_info', // カスタム投稿タイプの指定
+            'posts_per_page' => 5, // 表示する投稿数
         );
         $latest_shops = new WP_Query($args);
-        if ($latest_shops->have_posts()):
-            while ($latest_shops->have_posts()):
-                $latest_shops->the_post();
-                $thumbnail = get_field('store_image');
+        if ($latest_shops->have_posts()):  // 投稿があるかの確認
+            while ($latest_shops->have_posts()):  // ループ開始
+                $latest_shops->the_post();  // 現在の投稿データをセット
+                $thumbnail = get_field('store_image');  // カスタムフィールドから画像データを取得
         ?>
                 <div class="latest-info-item">  <!-- 各スライドとなるアイテム -->
-                    <?php if ($thumbnail): ?>
-                        <div class="latest-info-thumbnail">
-                            <img src="<?php echo esc_url($thumbnail['url']); ?>" alt="<?php echo esc_attr($thumbnail['alt']); ?>">
-                        </div>
-                    <?php else: ?>
-                        <div class="latest-info-thumbnail">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/no-image.png" alt="デフォルト画像">
-                        </div>
-                    <?php endif; ?>
-                    <h3><?php the_title(); ?></h3>
+                    <a href="<?php the_permalink(); ?>">  <!-- 投稿のパーマリンクをリンク先に設定 -->
+                        <?php if ($thumbnail): ?>
+                            <div class="latest-info-thumbnail">
+                                <img src="<?php echo esc_url($thumbnail['url']); ?>" alt="<?php echo esc_attr($thumbnail['alt']); ?>">
+                            </div>
+                        <?php else: ?>
+                            <div class="latest-info-thumbnail">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/no-image.png" alt="デフォルト画像">
+                            </div>
+                        <?php endif; ?>
+                    </a>
+                    <h3><?php the_title(); ?></h3>  <!-- 投稿タイトルを表示 -->
                 </div>
         <?php
             endwhile;
-            wp_reset_postdata();
+            wp_reset_postdata();  // ループ後のクリーンアップ
         endif;
         ?>
     </div>
