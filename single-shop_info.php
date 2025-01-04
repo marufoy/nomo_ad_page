72ccf6f42eb4ae3f28cb38115f3a82799fe6f3a7
ryomaruyama <marusin618@gmail.com>
2025-01-04 14:02:32 +0900

    [change] 詳細ページに画像を

--- single-shop_info.php
+++ single-shop_info.php
@@ -1,39 +1,49 @@
 <?php get_header(); ?>
 
 <div class="single-shop-info">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <!-- 店舗名 -->
+        <?php 
+        $store_name = get_field('store_name');
+        if ($store_name): ?>
+            <h2 class="store-name"><?php echo esc_html($store_name); ?></h2>
+        <?php endif; ?>
 
-    <?php
-    if (have_posts()) :
-        while (have_posts()) : the_post();
-            
-            // 店舗名を表示
-            $store_name = get_field('store_name'); 
-            if ($store_name) {
-                echo '<h2 class="store-name">' . esc_html($store_name) . '</h2>';
-            }
-            ?>
-            
-            <div class="shop-content">
-                <?php
-                // 店舗画像を表示
-                $store_image = get_field('store_image'); 
-                if ($store_image) {
-                    echo '<div class="custom-thumbnail">';
-                    echo '<img src="' . esc_url($store_image['url']) . '" alt="' . esc_attr($store_image['alt']) . '">';
-                    echo '</div>';
-                }
-                ?>
-                
-                <!-- 投稿本文を表示 -->
-                <div class="shop-description">
-                    <?php echo get_the_content(); ?>
+        <div class="shop-content">
+            <!-- メディアエリア -->
+            <div class="media-area">
+                <div class="custom-thumbnail">
+                    <?php
+                    $store_image = get_field('store_image');
+                    if ($store_image): ?>
+                        <img src="<?php echo esc_url($store_image['url']); ?>" alt="<?php echo esc_attr($store_image['alt']); ?>">
+                    <?php endif; ?>
+                </div>
+
+                <div class="detail-images">
+                    <?php
+                    $detail_images = array(
+                        get_field('detail_image_1'),
+                        get_field('detail_image_2'),
+                        get_field('detail_image_3'),
+                    );
+
+                    foreach ($detail_images as $detail_image):
+                        if ($detail_image): ?>
+                            <div class="detail-thumbnail">
+                                <img src="<?php echo esc_url($detail_image['url']); ?>" alt="<?php echo esc_attr($detail_image['alt']); ?>">
+                            </div>
+                        <?php endif;
+                    endforeach; ?>
                 </div>
             </div>
 
-        <?php endwhile;
-    endif;
-    ?>
-
+            <!-- 店舗説明 -->
+            <div class="shop-description">
+                <?php echo get_the_content(); ?>
+            </div>
+        </div>
+    <?php endwhile; endif; ?>
 </div>
 
 <?php get_footer(); ?>
