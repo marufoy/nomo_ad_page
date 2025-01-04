07738918b141681d41b2ae6872715d5b86da9d3b
ryomaruyama <marusin618@gmail.com>
2025-01-04 17:05:58 +0900

    [add]footerの追加と軽微な調整

--- footer.php
+++ footer.php
@@ -1,14 +1,27 @@
     <!-- フッター部分 -->
     <footer>
         <div class="site-footer">
-            <div class="footer-menu">
-                <?php
-                wp_nav_menu( array(
-                    'theme_location' => 'footer_menu', // フッターメニュー
-                    'menu_class'      => 'footer-menu',
-                ) );
-                ?>
+            <div class="footer-container">
+
+                <!-- フッターメニュー -->
+                <div class="footer-menu">
+                    <?php
+                    wp_nav_menu( array(
+                        'theme_location' => 'footer_menu', // フッターメニュー
+                        'menu_class'      => 'footer-menu',
+                    ) );
+                    ?>
+                </div>
+
+                <!-- ソーシャルリンク -->
+                <div class="footer-social">
+                    <a href="https://twitter.com/" target="_blank">Twitter</a>
+                    <a href="https://facebook.com/" target="_blank">Facebook</a>
+                    <a href="https://instagram.com/" target="_blank">Instagram</a>
+                </div>
             </div>
+
+            <!-- コピーライト -->
             <div class="copyright">
                 <p>&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</p>
             </div>
